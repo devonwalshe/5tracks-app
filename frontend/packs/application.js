e41881c93a22e55a6822fe29fc85640a28991de2
Azymuth <devon.walshe@gmail.com>
2018-04-09 15:32:09 +0100

    testing linting

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -6,6 +6,6 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import "components";
 
-console.log('Hello World from Webpacker')
-import 'components';
+console.log("Hello World from Webpacker");
